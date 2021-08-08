c12e16b9efe48576cc36539231ce206e3ce822ba
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-08 18:27:00 +0530

    [Anusha] Add: Calculate wage per month

--- EmployeeWageUC.py
+++ EmployeeWageUC.py
@@ -3,11 +3,17 @@
 @Date: 2021-08-05 12: 00: 30
 @Last Modified by: Anusha Manda
 @Last Modified time: 2021-08-06 18: 00: 30
-@Title: Check if Employee is present or absent
-        USing Switcher 
+@Title: Calculation wages for a month
+        
 '''
-
 import random
+
+IS_PRESENT_FULL = 1
+IS_PRESENT_HALF = 2
+EMP_RATE_PER_HR = 20
+MAX_NO_OF_WORKINHG_DAYS = 20
+totalWorkingDays = 0
+totalEmpHrs = 0
 
 
 def emp_hrs_check(empcheck):
@@ -19,16 +25,17 @@ def emp_hrs_check(empcheck):
         Return :
                 emp_hrs
     '''
-    switcher = {1: 8, 2: 4, 0: 0}
+    switcher = {1: 8, 2: 4, 0: 0}  # Here values are Working Hours
     return switcher[empcheck]
 
-
-IS_PRESENT_FULL_TIME = 1
-IS_PRESENT_PART_TIME = 2
-EMP_RATE_PER_HR = 20
-
 if __name__ == '__main__':
-    emp_check = random.randint(0, 2)
-    emp_hrs = emp_hrs_check(emp_check)
-    salary = emp_hrs * EMP_RATE_PER_HR
-    print(f"salary: {salary}")
+
+    while (totalWorkingDays < MAX_NO_OF_WORKINHG_DAYS):
+        totalWorkingDays += 1
+        emp_check = random.randint(0, 2)
+        emphrs = emp_hrs_check(emp_check)
+        #print(f"emphrs: {emphrs}")
+        totalEmpHrs = totalEmpHrs + emphrs
+        #print(totalEmpHrs)
+    totalsalary = totalEmpHrs * EMP_RATE_PER_HR
+    print(f"salary: Rs {totalsalary} ")
