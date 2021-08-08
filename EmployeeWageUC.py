f857776aeb67f71e3ea7d35bf4d65d1fae7e64e0
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-08 18:21:12 +0530

    [Anusha] Add : Fulltime and PartTime

--- EmployeeWageUC.py
+++ EmployeeWageUC.py
@@ -4,20 +4,27 @@
 @Last Modified by: Anusha Manda
 @Last Modified time: 2021-08-06 18: 00: 30
 @Title: Check if Employee is present or absent
-        employee work hours is 8
-        employee rate per hour is 20
+        Employee hours for full time is 8
+        Employee hours for part time is 4
 '''
-
 
 import random
 
+IS_PRESENT_FULL_TIME = 1
+IS_PRESENT_PART_TIME = 2
+EMP_RATE_PER_HR = 20
+
 if __name__=='__main__':
-    IS_PRESENT = 1
-    EMP_RATE_PER_HR = 20
-    emp_check = random.randint(0, 1)
-    if (emp_check == IS_PRESENT):
+    emp_check = random.randint(0, 2)
+    if(emp_check == IS_PRESENT_FULL_TIME):
         emp_hrs = 8
+        print("employee present full time")
+    elif(emp_check == IS_PRESENT_PART_TIME):
+        emp_hrs = 4
+        print("Employee is present half time")
     else:
         emp_hrs = 0
-    salary = (EMP_RATE_PER_HR * emp_hrs)
-    print(f"salary:{salary}")
+        print("Employee is absent")
+
+salary = emp_hrs * EMP_RATE_PER_HR
+print("salary: "+str(salary))
